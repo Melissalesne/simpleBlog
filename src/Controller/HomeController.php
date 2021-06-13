5c36d9e41644ea8e6a282a2d7fbdbe9ee0fab501
Melissa Lesne <melissalesne@orange.fr>
2021-06-13 09:41:00 +0200

    Ajout des categories

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,21 +14,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     private $repoArticle;
+    private $repoCategory;
 
-    public function __construct(ArticleRepository $repoArticle)
+    public function __construct(ArticleRepository $repoArticle, CategoryRepository $repoCategory)
     {
         $this->repoArticle = $repoArticle;
+        $this->repoCategory = $repoCategory;
     }
     /**
      * @Route("/", name="home")
      */
     public function index(): Response
     {
-
+        $categories = $this->repoCategory->findAll();
         // Récupère tout les articles
         $articles = $this->repoArticle->findAll();
         return $this->render("home/index.html.twig", [
             'articles' => $articles,
+            'categories' => $categories
         ]);
     }
     /**
@@ -41,6 +46,23 @@ class HomeController extends AbstractController
 
         return $this->render("show/index.html.twig", [
             'article' => $article,
+        ]);
+    }
+
+    /**
+     * @Route("/showArticles/{id}", name="show_article")
+     */
+    public function showArticle(?Category $category): Response
+    {
+        if ($category) {
+            $articles = $category->getArticles()->getValues();
+        } else {
+            return $this->redirectToRoute('home');
+        }
+        $categories = $this->repoCategory->findAll();
+        return $this->render("home/index.html.twig", [
+            'articles' => $articles,
+            'categories' => $categories
         ]);
     }
 }
